fe6a6b6a667284d1232018a756751728d63b91d2
jyakaranda <1217358645@qq.com>
2019-04-23 16:15:33 +0800

    init

--- LeGO-LOAM/include/lego/utility.h
+++ LeGO-LOAM/include/lego/utility.h
@@ -52,11 +52,19 @@ typedef pcl::PointXYZI  PointType;
 
 // VLP-16
 extern const int N_SCAN = 16;
-extern const int Horizon_SCAN = 1800;
-extern const float ang_res_x = 0.2;
+extern const int Horizon_SCAN = 3600;   // 5Hz: 3600, 10Hz: 1800
+extern const float ang_res_x = 0.1;     // 5Hz: 0.1,  10Hz: 0.2
 extern const float ang_res_y = 2.0;
 extern const float ang_bottom = 15.0+0.1;
 extern const int groundScanInd = 7;
+
+// HDL-32E
+// extern const int N_SCAN = 32;
+// extern const int Horizon_SCAN = 1800;
+// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+// extern const float ang_res_y = 41.33/float(N_SCAN-1);
+// extern const float ang_bottom = 30.67;
+// extern const int groundScanInd = 20;
 
 // Ouster OS1-64
 // extern const int N_SCAN = 64;
@@ -66,10 +74,10 @@ extern const int groundScanInd = 7;
 // extern const float ang_bottom = 16.6+0.1;
 // extern const int groundScanInd = 15;
 
-extern const bool loopClosureEnableFlag = false;
+extern const bool loopClosureEnableFlag = true;
 extern const double mappingProcessInterval = 0.3;
 
-extern const float scanPeriod = 0.1;
+extern const float scanPeriod = 0.2;
 extern const int systemDelay = 0;
 extern const int imuQueLength = 200;
 extern const string imuTopic = "/imu/data";
